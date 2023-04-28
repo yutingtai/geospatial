374c3f5b09720a03d87f73051c2763e236ed82e0
yutingtai <108973615+yutingtai@users.noreply.github.com>
2023-04-28 17:05:48 +0800

    Delete extract_xyz_from_tif directory

--- extract_xyz_from_tif/scatter_plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df = pd.read_csv('./level_location.csv', usecols=['LAT', 'LON', 'VALUE_levelling','value_InSAR'])
-x = df['VALUE_levelling']
-y = df['value_InSAR']
-
-plt.scatter(x, y)
-plt.show()
